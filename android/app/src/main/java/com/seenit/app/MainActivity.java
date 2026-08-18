2e221e7bf9913418457661c8e709a1a05325dba5
Julian <JulianFouillade@gmail.com>
2026-08-18 15:49:54 +0000

    fix(auth): enable WebView multi-window popups, add android google-services.json, and remove redirect fallback (v1.1.6)

--- android/app/src/main/java/com/seenit/app/MainActivity.java
+++ android/app/src/main/java/com/seenit/app/MainActivity.java
@@ -1,5 +1,24 @@
 package com.seenit.app;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        try {
+            WebView webView = this.getBridge().getWebView();
+            if (webView != null) {
+                WebSettings settings = webView.getSettings();
+                settings.setJavaScriptCanOpenWindowsAutomatically(true);
+                settings.setSupportMultipleWindows(true);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
