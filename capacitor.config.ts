79afd576265fb3c2b98a0ea47580ea5a6e265e76
Julian <JulianFouillade@gmail.com>
2026-08-18 09:30:12 +0000

    Fix Android 12 Splash Screen theme and enable autoHide

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -7,12 +7,13 @@ const config: CapacitorConfig = {
   // Configuration pour le Live Web View (Pointer vers l'URL de production)
   server: {
     url: 'https://ais-pre-mooctibtw2amkshvkzlqij-700628279309.europe-west2.run.app',
-    cleartext: true
+    cleartext: true,
+    androidScheme: 'https'
   },
   plugins: {
     SplashScreen: {
-      launchShowDuration: 10000, // Durée longue, on le cache manuellement depuis le code React
-      launchAutoHide: false, // Ne pas cacher automatiquement, pour éviter un écran blanc
+      launchShowDuration: 1600,
+      launchAutoHide: true,
       backgroundColor: '#040406',
       androidSplashResourceName: 'splash',
       androidScaleType: 'CENTER_CROP',
