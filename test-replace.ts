5917de428125e8164afaf484237d171f9b1f47d6
AI Studio <JulianFouillade@gmail.com>
2026-08-26 07:42:41 +0000

    chore: clean test files

--- test-replace.ts
+++
@@ -1,2 +0,0 @@
-const t1 = "Marvel's Daredevil".replace(/^(Marvel's |DC's )/i, '');
-console.log(t1);
--- test-tmdb.ts
+++
@@ -1,7 +0,0 @@
-import { tmdb } from './src/features/shows/tmdb';
-
-async function run() {
-  const res = await tmdb.searchMulti('Daredevil');
-  console.log(res.value?.results.map((r: any) => r.name || r.title));
-}
-run();
