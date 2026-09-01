a902d7b1ce008c26c0b61d3cd330fe72199ca09a
julfou7 <julianfouillade@gmail.com>
2026-09-02 00:06:37 +0200

    refactor(config): préserver les invariants Android vérifiables

--- src/config/seenit.ts
+++ src/config/seenit.ts
@@ -1,5 +1,1 @@
-export const SEENIT_APP_ID = 'com.seenit.app';
-export const SEENIT_APP_NAME = 'SeenIt';
-export const SEENIT_API_ORIGIN = 'https://seenit.ai.studio';
 export const SEENIT_FIREBASE_PROJECT_ID = 'gen-lang-client-0201895414';
-export const SEENIT_FIRESTORE_DATABASE_ID = 'default';
