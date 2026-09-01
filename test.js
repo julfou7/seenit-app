02cc0e3bd814bf31486a5861c08e0ee8da549f72
julfou7 <julianfouillade@gmail.com>
2026-09-02 00:01:20 +0200

    chore(dépôt): supprimer le brouillon JavaScript

--- test.js
+++
@@ -1,6 +0,0 @@
-const code = `
-  const [runtime, setRuntime] = useState<number | null>((show as any).runtime || null);
-  const [releaseYear, setReleaseYear] = useState<string | null>(
-    show.firstAirDate ? show.firstAirDate.slice(0, 4) : null
-  );
-`;
