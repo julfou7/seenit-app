ab7d106ed5cff538297bb797f2fa327cd896bc4c
julfou7 <julianfouillade@gmail.com>
2026-09-01 23:58:39 +0200

    refactor(config): centraliser les constantes SeenIt

---
+++ src/config/seenit.ts
@@ -0,0 +1,5 @@
+export const SEENIT_APP_ID = 'com.seenit.app';
+export const SEENIT_APP_NAME = 'SeenIt';
+export const SEENIT_API_ORIGIN = 'https://seenit.ai.studio';
+export const SEENIT_FIREBASE_PROJECT_ID = 'gen-lang-client-0201895414';
+export const SEENIT_FIRESTORE_DATABASE_ID = 'default';
