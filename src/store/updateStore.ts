bf18af0ac8277b206ed15fad339eafeeeebf27b9
julfou7 <julianfouillade@gmail.com>
2026-09-05 21:59:27 +0200

    chore: préparer la release APK 1.4.117
    Changelog: aucun
    
    Détails techniques:
    - Aligne les huit surfaces de version en 1.4.117 / 104117.

--- src/store/updateStore.ts
+++ src/store/updateStore.ts
@@ -9,7 +9,7 @@ import {
   type SeenItReleaseInfo
 } from '../features/release/releasePolicy';
 
-export const CURRENT_APP_VERSION = '1.4.116';
+export const CURRENT_APP_VERSION = '1.4.117';
 export type AppReleaseInfo = SeenItReleaseInfo;
 
 interface UpdateState {
