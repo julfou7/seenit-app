a6b980e312e806fcb1a5369880287ae00e3a0a05
julfou7 <julianfouillade@gmail.com>
2026-09-06 09:45:28 +0200

    chore: préparer la release APK 1.4.118 (#163)
    Prépare atomiquement les huit surfaces de version SeenIt pour la publication 1.4.118.

--- server.ts
+++ server.ts
@@ -572,7 +572,7 @@ async function startServer() {
 
       const headers: Record<string, string> = {
         'X-Plex-Product': 'SeenIt',
-        'X-Plex-Version': '1.4.117',
+        'X-Plex-Version': '1.4.118',
         'X-Plex-Client-Identifier': plexClientId,
         'Accept': 'application/json'
       };
