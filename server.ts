575c02c6b520add641d51840abedf90dd7099628
julfou7 <julianfouillade@gmail.com>
2026-09-09 23:39:53 +0200

    chore(release): préparer SeenIt 1.4.134 (#249)
    Aligne atomiquement les 8 surfaces canoniques de version pour publier le correctif UX des carrousels À Regarder.
    
    Changelog: aucun
    
    Refs #102 #229

--- server.ts
+++ server.ts
@@ -577,7 +577,7 @@ async function startServer() {
 
       const headers: Record<string, string> = {
         'X-Plex-Product': 'SeenIt',
-        'X-Plex-Version': '1.4.133',
+        'X-Plex-Version': '1.4.134',
         'X-Plex-Client-Identifier': plexClientId,
         'Accept': 'application/json'
       };
