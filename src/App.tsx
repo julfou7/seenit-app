f8af81f67b2dea260f2d604eacf384abc323c51a
julfou7 <julianfouillade@gmail.com>
2026-09-11 15:20:16 +0200

    fix(explorer): préserver l’écran lors des changements d’onglet

--- src/App.tsx
+++ src/App.tsx
@@ -508,11 +508,12 @@ function MainApp() {
               )}
 
               {mountedTabs.has('discover') && (
-                <Activity mode={currentTab === 'discover' ? 'visible' : 'hidden'}>
-                  <div className="flex-1 min-h-0 flex flex-col">
-                    <DiscoverScreen onShowClick={openTmdbMedia} />
-                  </div>
-                </Activity>
+                <div
+                  className={currentTab === 'discover' ? 'flex-1 min-h-0 flex flex-col' : 'hidden'}
+                  aria-hidden={currentTab !== 'discover'}
+                >
+                  <DiscoverScreen onShowClick={openTmdbMedia} />
+                </div>
               )}
 
               {mountedTabs.has('downloads') && (
